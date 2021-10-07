a67996244b8a1b0f3a477b048a8d5ebd272e92e8
Yosrabej <79087257+Yosrabej@users.noreply.github.com>
2021-10-07 10:05:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 
-<?php include 'header.php';?>
-<?php include 'utils.php ?>
-<?php include 'footer.php';?>
+<?php include ('header.php');?>
+<?php include ('utils.php'); ?>
+<?php include ('footer.php');?>
